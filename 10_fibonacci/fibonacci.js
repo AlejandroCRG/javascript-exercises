554906913cf76f8f19280cdf278e64c46245f6e4
AlejandroCRG <alejandrocrg1@gmail.com>
2022-12-12 11:14:19 +0100

    Fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    fib = [1, 1]
+    let i = 1
+    while (i <= 25){
+        let result = 0
+        result = fib[i] + fib[i-1]
+        fib.push(result)
+        i++
+    }
+    if (Math.sign(n) === -1){
+        return 'OOPS'
+    }
+    return fib[n-1]
 };
 
 // Do not edit below this line
