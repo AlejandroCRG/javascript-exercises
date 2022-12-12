8042e916ff93275a79eb12bbab062497f4880052
AlejandroCRG <alejandrocrg1@gmail.com>
2022-12-12 18:11:50 +0100

    Done everything

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce(function(a, b){
+        const aage = getAge(a.yearOfBirth, a.yearOfDeath)
+        const bage = getAge(b.yearOfBirth, b.yearOfDeath)
+        return aage < bage ? b : a;
+    })
 };
+
+function getAge(a, b){
+    if (!b){
+        b = new Date().getFullYear();
+    }
+    return b-a
+}
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
