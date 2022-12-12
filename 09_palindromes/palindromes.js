2df6730c9069febbbac456ba052470b3af9634bc
AlejandroCRG <alejandrocrg1@gmail.com>
2022-12-12 10:43:11 +0100

    Palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    let str1 = string.toLowerCase()
+    let str2 = str1.replace(/[^\w\s\']|_/g, "")
+    let str3 = str2.replace(/\s+/g, "");
+    let i = (str3.length-1)
+    let finalString = ''
+    while (i >= 0){
+        finalString = finalString + str3[i];
+        i--;
+    }
+    if (finalString === str3){
+        return true
+    }
+    else return false
+}
 
 // Do not edit below this line
 module.exports = palindromes;
