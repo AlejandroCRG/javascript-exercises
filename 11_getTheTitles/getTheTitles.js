9ee3fac5aa64c2f7cbb6e756170794150b200953
AlejandroCRG <alejandrocrg1@gmail.com>
2022-12-12 14:23:12 +0100

    nothing done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = function(dict) {
+    dict.filter(obj => obj.title)
+    }
 
-};
 
 // Do not edit below this line
 module.exports = getTheTitles;
